7fdd31d20b539ded26155b3195bfb905d633693a
“ChrisAle117” <20223tn105@utez.edu.mx>
2025-11-11 16:26:08 -0600

    waos2

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ async function openCamera() {
         const constraints = {
             video: {
                 facingMode: useFrontCamera ? 'user' : 'environment',
-                width: { ideal: 320 },
-                height: { ideal: 240 }
+                width: { ideal: 1280 },
+                height: { ideal: 720 }
             }
         };
         stream = await navigator.mediaDevices.getUserMedia(constraints);
